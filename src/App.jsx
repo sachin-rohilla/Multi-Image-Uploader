cb7c05966e0ed911ca3b74b807dd3b2e2e3200bd
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-06 16:13:15 +0530

    edit-btn-add

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,9 @@ function App() {
   console.log("img", image);
   return (
     <>
-      <div className="px-4 py-8 border w-[90%] mx-auto my-8 shadow-md rounded-lg flex flex-wrap justify-center items-center gap-4 ">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]?.map((item, index) => (
+      <h1 className="text-center my-4 font-semibold text-xl">Image Uploader</h1>
+      <div className="px-4 py-8 border w-[90%] mx-auto mb-8 shadow-md rounded-lg flex flex-wrap justify-center items-center gap-4 ">
+        {Array.from({ length: 10 })?.map((item, index) => (
           <div className="relative w-52 h-52" key={index}>
             <div className=" w-full h-full border-2 overflow-hidden border-dotted border-[#a78bfa] flex items-center justify-center rounded-lg ">
               {image?.length > 0 &&
@@ -54,12 +55,12 @@ function App() {
                     className="text-2xl cursor-pointer  absolute top-2 right-2"
                     onClick={() => handleClose(index + 1)}
                   />
-                  {/* <MdEdit
+                  <MdEdit
                     className="text-2xl cursor-pointer  absolute top-8 right-2"
                     onClick={() => {
                       setIsEdit(true);
                     }}
-                  /> */}
+                  />
                 </div>
               )}
           </div>
